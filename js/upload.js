40e981b220afc810390eb9fa95a1dfa00db4f9ac
Rainik777 <Rainik_777@mail.ru>
2019-10-10 15:44:30 +0300

    пин поехал, но слишком далеко

--- js/upload.js
+++ js/upload.js
@@ -8,8 +8,6 @@
   var SLIDER_PIN = IMG_SETUP.querySelector('.effect-level__pin');
   var SLIDER_LINE = IMG_SETUP.querySelector('.effect-level__depth');
   var SLIDER_BLOCK = IMG_SETUP.querySelector('.effect-level__line');
-  var PIN_MIN_X = 595;
-  var PIN_MAX_X = 1045;
   // var EFFECTS = IMG_SETUP.querySelectorAll('.effects__item');
   // var IMG_PREVIEW = IMG_SETUP.querySelector('.img-upload__preview');
   // var TAG_INPUT = IMG_SETUP.querySelector('input[name="hashtags"]');
@@ -30,16 +28,18 @@
     document.removeEventListener('keydown', onImgSetupEscPress);
   };
 
-  var getSliderPosition  = function (clientX) {
-    var clientXcurrent = Math.floor((clientX - PIN_MIN_X) * 100 / (PIN_MAX_X - PIN_MIN_X));
+  /* var getSliderPosition  = function (evt) {
+    var PIN_MIN_X = 595;
+    var PIN_MAX_X = 1045;
+    var clientXcurrent = Math.floor((evt.clientX - PIN_MIN_X) * 100 / (PIN_MAX_X - PIN_MIN_X));
     SLIDER_PIN.style.left = clientXcurrent + '%';
     SLIDER_LINE.style.width = clientXcurrent + '%';
-    // console.log(clientXcurrent);
-  };
+    console.log(evt.clientX);
+  }; */
 
-  var setupSliderPosition = function (evt) {
-    getSliderPosition(evt.clientX);
-  };
+  // var setupSliderPosition = function (evt) {
+  //   getSliderPosition(evt.clientX);
+  // };
 
  /* var sliderEffect = function () {
     ----- Здесь должна быть функция слайдера
@@ -53,13 +53,12 @@
   // ЗАКРЫВАЕМ блок редактирования изображения
   BTN_CLOSE_IMG_SETUP.addEventListener('click', closeImgSetup);
 
-  SLIDER_BLOCK.addEventListener('click', setupSliderPosition);
+  // SLIDER_BLOCK.addEventListener('click', getSliderPosition);
 
   // перемещение пина вслед за мышкой - НЕ РАБОТАЕТ
   SLIDER_PIN.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
     var startClientX = evt.clientX;
-    console.log('mousedown' + ' ' + startClientX);
 
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
@@ -67,6 +66,7 @@
       startClientX = moveEvt.clientX;
 
       SLIDER_PIN.style.left = (SLIDER_PIN.offsetLeft - shiftClientX) + 'px';
+      SLIDER_LINE.style.width = (SLIDER_PIN.offsetLeft - shiftClientX) + 'px';
     };
 
     var onMouseUp = function (upEvt) {
