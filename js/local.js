e09e724b5557ca8e109c3a6a4e062967864ca303
Rainik777 <Rainik_777@mail.ru>
2019-10-22 15:08:25 +0300

    воздушные формы

--- js/local.js
+++ js/local.js
@@ -5,6 +5,7 @@
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
   var fileInput = document.querySelector('#upload-file');
+
   fileInput.addEventListener('change', function () {
     var file = fileInput.files[0];
     if (file) {
@@ -15,12 +16,14 @@
       if (matches) {
         var reader = new FileReader();
         var preview = document.querySelector('.img-upload__preview');
+
         reader.addEventListener('load', function () {
           preview.firstElementChild.src = reader.result;
         });
         reader.addEventListener('error', function () {
           window.onError(reader.error.message);
         });
+
         reader.readAsDataURL(file);
       }
     }
